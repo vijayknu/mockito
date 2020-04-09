afcf0cbc8807514437d4755111e087608f745c91
Namboothiri, Vijaya <Vijaya.Namboothiri@Clarivate.com>
2020-04-09 21:02:42 +0530

    Section 2

---
+++ src/test/java/com/nad/tm/junitdemo/helper/AllTests.java
@@ -0,0 +1,11 @@
+package com.nad.tm.junitdemo.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ QuickBeforeAfterTest.class, StringHelperTest.class })
+public class AllTests {
+
+}
--- src/test/java/com/nad/tm/junitdemo/helper/StringHelperParameterizedTest.java
+++ src/test/java/com/nad/tm/junitdemo/helper/StringHelperParameterizedTest.java
@@ -2,6 +2,7 @@ package com.nad.tm.junitdemo.helper;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.Test;
@@ -14,38 +15,26 @@ public class StringHelperParameterizedTest {
 
 	private final static StringHelper stringHelper = new StringHelper();
 	
+	private String input;
+	private String output;
+	
+	
+	public StringHelperParameterizedTest(String input, String output) {
+		this.input = input;
+		this.output = output;
+	}
+
 	@Parameters
-	public static Collection testConditions() {
-		return null;
+	public static Collection<String[]> testConditions() {
+		String output[][] ={
+					{"AACD", "CD"},
+					{"ACD", "CD"}
+				};
+		return Arrays.asList(output);
 	}
 	
 	@Test
 	public void testTruncateAInFirst2Positions_Ainfirst2Positions() {
-		assertEquals("CD",stringHelper.truncateAInFirst2Positions("AACD"));
-	}
-	
-	@Test
-	public void testTruncateAInFirst2Positions_Ainlast2Positions() {
-		assertEquals("CDAA",stringHelper.truncateAInFirst2Positions("CDAA"));
-	}
-
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_TC_001() {
-		assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("AB"));
-	}
-
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_TC_002() {
-		assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("A"));
-	}
-	
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_TC_003() {
-		assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
-	}
-	
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_TC_004() {
-		assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+		assertEquals(output,stringHelper.truncateAInFirst2Positions(input));
 	}
 }
